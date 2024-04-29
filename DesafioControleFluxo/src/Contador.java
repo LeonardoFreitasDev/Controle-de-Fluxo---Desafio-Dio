7c32b54c93b8f59b9bdca1c019409aa59ab3195f
LeonardoFreitasDev <leonardo.lfs18@gmail.com>
2024-04-29 14:19:41 -0300

    Corrção do warnning de fechamento do sscanner

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -16,6 +16,8 @@ public class Contador {
 			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
             System.out.println("O segundo parâmetro deve ser maior que o primeiro");
 		}
+
+        terminal.close();
 		
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
